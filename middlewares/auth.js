2f5007d19eb81ea34f465ef82654aae59ab9543e
Varun-Kolanu <kolanuvarun739@gmail.com>
2023-10-17 07:58:06 +0530

    [UPDATED] Error Handling

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,16 +3,20 @@ import jwt from "jsonwebtoken";
 import ErrorHandler from "./error.js";
 
 const isAuthenticated = async (req, res, next) => {
-    const token = req.headers.authorization;
+    try {
+        const token = req.headers.authorization;
 
-    if (!token || !token.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        if (!token || !token.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const tokenValue = token.replace('Bearer ', '');
+        const decoded = jwt.verify(tokenValue, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded);
+        next();
+    } catch (error) {
+        next(new ErrorHandler(error.message, 401));
     }
-
-    const tokenValue = token.replace('Bearer ', '');
-    const decoded = jwt.verify(tokenValue, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded);
-    next();
 }
 
 export default isAuthenticated;
--- middlewares/error.js
+++ middlewares/error.js
@@ -12,7 +12,6 @@ export const errorMiddleware = (err,req,res,next) => {
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
-        stack: err.stack
     });
 }
 
